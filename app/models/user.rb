63c57c37bbec93be3e846c1ae5ed69bcd2396e31
杨昊霖 <yanghaolin@yanghaolindeMacBook-Pro.local>
2017-08-05 14:36:31 +0800

    token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+  before_create :generate_authentication_token
+
+  def generate_authentication_token
+    self.authentication_token = Devise.friendly_token
+  end
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
